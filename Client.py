3b0a864e7e6f2caa06f7c625af80a52d199c111e
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2019-01-25 03:47:24 +0200

    Step 4 - add auth

--- Client.py
+++ Client.py
@@ -1,8 +1,14 @@
+import datetime
 import json
 import threading
 import RC5
 
 class Client(threading.Thread):
+    secure_data = {"identfier" : "secure message"}
+    logins = {"user1" : "pass1", "user2" : "pass2"}
+    wrongs = {}
+    bans = {}
+
     def __init__(self, ip, port, connection):
         threading.Thread.__init__(self)
         self.connection = connection
@@ -27,6 +33,58 @@ class Client(threading.Thread):
                 # получаем оригинальный хеш пароля
                 password = (xorPass.encode() ^ 1100).decode()
 
+                # проверяем на бан
+                if login in list(self.bans.keys()):
+                    # проверяем актуальность бана и если он уже не актуален чистим базу банов и пускаем
+                    delta = datetime.date.today() - self.bans[login]
+                    if delta >= 0:
+                        del self.bans[login]
+                        # сверяем данные, если все верно - пускаем дальше
+                        if login in list(self.logins.keys()):
+                            if self.logins[login] == "password":
+                                # возвращаем ответ-разрешение
+                                self.connection.sendall(RC5.RC5.encryptBytes('{"authaccess" : { "status" : "true"}}'))
+                                # чистим список ошибочных логинов
+                            else:
+                                if login in list(self.wrongs.keys()):
+                                    if self.wrongs[login] <= 3:
+                                        # меняем счетчик
+                                        self.wrongs[login] = self.wrongs[login] + 1
+                                    else:
+                                        # баним
+                                        timestamp = datetime.date.today() + datetime.timedelta(seconds=60)
+                                        self.bans.update('{' + login + ' : ' + timestamp + '}')
+                                else:
+                                    # добавляем счетшик ошибок
+                                    self.wrongs.update('{' + login + ' : 1 }')
+
+                                    # возвращаем ответ-запрет
+                                    self.connection.sendall(
+                                        RC5.RC5.encryptBytes('{"authaccess" : { "status" : "false"}}'))
+                    else:
+                        self.connection.sendall(RC5.RC5.encryptBytes('{"authaccess" : { "status" : "ban", "time" : ' + delta + '}}'))
+                else:
+                    # сверяем данные, если все верно - пускаем дальше
+                    if login in list(self.logins.keys()):
+                        if self.logins[login] == "password":
+                            # возвращаем ответ-разрешение
+                            self.connection.sendall(RC5.RC5.encryptBytes('{"authaccess" : { "status" : "true"}}'))
+                            # чистим список ошибочных логинов
+                        else:
+                            if login in list(self.wrongs.keys()):
+                                if self.wrongs[login] <= 3:
+                                    # меняем счетчик
+                                    self.wrongs[login] = self.wrongs[login] + 1
+                                else:
+                                    # баним
+                                    timestamp = datetime.date.today() + datetime.timedelta(seconds=60)
+                                    self.bans.update('{' + login + ' : ' + timestamp + '}')
+                            else:
+                                # добавляем счетшик ошибок
+                                self.wrongs.update('{' + login + ' : 1 }')
+
+                                # возвращаем ответ-запрет
+                                self.connection.sendall(RC5.RC5.encryptBytes('{"authaccess" : { "status" : "false"}}'))
 
             elif list(jData.keys())[0] == "postcode":
                 # проверка почтового кода
